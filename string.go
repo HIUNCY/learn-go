19dfbccf26895421230f0eee5d54aa6ab3a101b5
HIUNCY <wolesajay@gmail.com>
2024-02-25 14:36:14 +0700

    Data Type: String

---
+++ string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Muhamad")
+	fmt.Println("Zainul")
+	fmt.Println("Kamal")
+
+	fmt.Println(len("Ajay"))
+	fmt.Println("Ajay"[1]) // still byte, fix in next learn: variable
+}
