4bec62fefc545f39deeaed2b8e32fcc41f308f2a
HIUNCY <wolesajay@gmail.com>
2024-02-26 00:46:06 +0700

    Comparison Operator

--- comparison.go
+++ comparison.go
@@ -6,17 +6,17 @@ func main() {
 	name1 := "Ajay"
 	name2 := "Ajay"
 
-	result1 := name1 == name2 // are name1 equal to name2? returning true
-	fmt.Println(result1)
-	result2 := name1 != name2 // are name1 not equal to name2? returning false
-	fmt.Println(result2)
+	result := name1 == name2 // are name1 equal to name2? returning true
+	fmt.Println(result)
+	result = name1 != name2 // are name1 not equal to name2? returning false
+	fmt.Println(result)
 
 	number1 := 10
 	number2 := 7
 
-	result3 := number1 > number2 // is number1 greater than number2? returning true
-	fmt.Println(result3)
-	result4 := number1 < number2 // is number1 less than number2? returning false
-	fmt.Println(result4)
+	result = number1 > number2 // is number1 greater than number2? returning true
+	fmt.Println(result)
+	result = number1 < number2 // is number1 less than number2? returning false
+	fmt.Println(result)
 	// note: <= for "less than or equal", and >= for "greater than or equal"
 }
