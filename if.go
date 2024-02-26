b7b41f966ff86d544dac5229f9ffe2a0e6c89f74
HIUNCY <wolesajay@gmail.com>
2024-02-26 18:52:55 +0700

    If Expression

---
+++ if.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Zainul"
+
+	if name == "Ajay" {
+		fmt.Println("Hello, Ajay!")
+	} else if name == "Zainul" {
+		fmt.Println("Hello, Zainul!")
+	} else {
+		fmt.Println("May I know u?")
+	}
+
+}
